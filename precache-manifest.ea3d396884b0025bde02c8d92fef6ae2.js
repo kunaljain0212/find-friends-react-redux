94e9a46c088104d0f1b5342b3d21b20339b59323
Kunal Jain <jaikunal209@gmail.com>
2021-01-04 23:47:23 +0530

    Updates

---
+++ precache-manifest.ea3d396884b0025bde02c8d92fef6ae2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922fe1434df31ff3d05119f5f54d0290",
+    "url": "/find-friends-react-redux/index.html"
+  },
+  {
+    "revision": "980a93a3d2c91f418ceb",
+    "url": "/find-friends-react-redux/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "bb5845e88fccdc1ee2c1",
+    "url": "/find-friends-react-redux/static/css/main.f206a7cf.chunk.css"
+  },
+  {
+    "revision": "980a93a3d2c91f418ceb",
+    "url": "/find-friends-react-redux/static/js/2.529e050c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/find-friends-react-redux/static/js/2.529e050c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5845e88fccdc1ee2c1",
+    "url": "/find-friends-react-redux/static/js/main.ffe45685.chunk.js"
+  },
+  {
+    "revision": "a19a8ca7cd361584349a",
+    "url": "/find-friends-react-redux/static/js/runtime-main.db842410.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/find-friends-react-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
